08d2cf12ef76a47f5d7bb0457d60f887256d7c29
abelass <rainer@websimple.be>
2017-07-05 11:10:46 +0200

    cosmétique

--- inc/reservations_formulaires.php
+++ inc/reservations_formulaires.php
@@ -13,12 +13,11 @@
  * Récupère les définitions des promotions.
  *
  * @param string $type_promotion
- *      Le Type de promotion.
+ *        	Le Type de promotion.
  * @param array $valeurs
- *      Les valeurs par défaut
+ *        	Les valeurs par défaut
  *
- * @return array
- *      défintion en format du plugin saisies.
+ * @return array défintion en format du plugin saisies.
  */
 function reservations_formulaires_definition_saisies($type, $valeurs) {
 
@@ -26,7 +25,6 @@ function reservations_formulaires_definition_saisies($type, $valeurs) {
 	$configurations_defs = find_all_in_path("formulaire_configurations/", '^');
 
 	$configurations_noms = array();
-	$configurations = array();
 
 	if (is_array($configurations_defs)) {
 
@@ -34,16 +32,14 @@ function reservations_formulaires_definition_saisies($type, $valeurs) {
 			list($nom, $extension) = explode('.', $fichier);
 			// Charger la définition des champs
 			if ($confs = charger_fonction($nom, "formulaire_configurations", true)) {
-				$configuration = $confs($valeurs);
-				if ($type == $nom and isset($configuration['saisies'])) {
-					$configurations[]= array(
-							'saisie' => 'fieldset',
-							'options' => array(
-								'nom' => 'specifique',
-								'label' => _T('reservation_formulaire_configuration:champ_configuration_label'),
-							),
+				if ($type == $nom and $configuration = $confs($valeurs) and isset($configuration['saisies'])) {
+					$configurations[] = array(
+						'saisie' => 'fieldset',
+						'options' => array(
+							'nom' => 'specifique',
+							'label' => _T('reservation_formulaire_configuration:champ_configuration_label')
+						),
 						'saisies' => $configuration['saisies']
-
 					);
 				}
 				// Lister les promotions dipsonibles
@@ -53,19 +49,18 @@ function reservations_formulaires_definition_saisies($type, $valeurs) {
 		}
 	}
 
-	$defaut = array('saisies' => array (
-		'saisie' => 'selection',
-		'options' => array (
-			'nom' => 'type',
-			'label' => _T('reservation_formulaire_configuration:champ_type_label'),
-			'datas' => $configurations_noms,
+	$defaut = array(
+		'saisies' => array(
+			'saisie' => 'selection',
+			'options' => array(
+				'nom' => 'type',
+				'label' => _T('reservation_formulaire_configuration:champ_type_label'),
+				'datas' => $configurations_noms
+			)
 		)
-	));
+	);
 
 	$saisies = array_merge($defaut, $configurations);
-
-	$saisies['champs_extras'] = $configuration['champs_extras'];
-
 
 	return $saisies;
 }
