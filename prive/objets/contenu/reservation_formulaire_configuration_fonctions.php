37dee944600e33394eef1b0661f32ad3d689d652
abelass <rainer@websimple.be>
2017-08-28 14:10:59 +0200

    utilité pas claire de cette fonction

--- prive/objets/contenu/reservation_formulaire_configuration_fonctions.php
+++ prive/objets/contenu/reservation_formulaire_configuration_fonctions.php
@@ -6,20 +6,15 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 include_spip('inc/reservations_formulaires');
 include_spip('inc/saisies');
 
-function reservations_formulaires_traduire_configuration($type, $configuration) {
+/*
+ * no utilisñe, verifier sens
+ * function reservations_formulaires_traduire_configuration($type, $configuration) {
 
 	$saisies = reservations_formulaires_charger_definitions($type);
 
 	$configuration = json_decode($configuration, true);
 
-
 	$saisies = saisies_lister_par_nom($saisies['saisies']['saisies']);
-
-	print '<pre>';
-	print_r($saisies);
-	print '</pre>';
-
-
 
 	$saisies_labels = array();
 
@@ -33,24 +28,15 @@ function reservations_formulaires_traduire_configuration($type, $configuration) 
 			$saisies_labels[$index]['name'] = $values['options']['nom'];
 			$saisies_labels[$index['options']['nom']]['values'] = $values['options']['label'];
 		}
-
 	}
-
-	print '<pre>';
-	print_r($saisies_labels, false);
-	print '</pre>';
-	//print_r($configuration);
 
 	foreach($configuration AS $champ => $valeur) {
 		$conf['label'] = $saisies['noms'][$type];
-		//print_r($champ);		//print_r($valeur);
+
 		if(isset($saisies_labels[$champ])) {
-			print_r($saisies_labels[$champ]);
 			$conf[][$saisies_labels[$champ]] = $valeur;
-			//unset($configuration[$champ]);
 		}
 	}
 
-
 	return $conf;
-}
+}*/
